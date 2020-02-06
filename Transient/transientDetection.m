ab83493ddab41b907f7d36e8498661f528b1c216
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-02-06 10:41:18 +0000

    working of RFF

--- Transient/transientDetection.m
+++ Transient/transientDetection.m
@@ -2,19 +2,35 @@
     first implementation of transient detection
 %}
 
-% getting the upper envelope
+%% getting the upper envelope
 clc; clear; close all;
 load('data');
 
 upperEnv = [];
 counter = 1;
 hEnvMean = mean(hEnv);
+maxValue = max(hEnv);
+maxPointReached = false;
+
 for k = 1:length(hEnv)
     if hEnv(k) >= hEnvMean
-       upperEnv(counter) = hEnv(k);
-       counter = counter + 1;
+        if hEnv(k) == maxValue
+            maxPointReached = true;
+        end
+        
+        if maxPointReached 
+           if hEnv(k) >=
+        else
+            upperEnv(counter) = hEnv(k);
+            counter = counter + 1; 
+        end
+       
     end
 end
+
+% trimming off extra data
+
+
 
 figure(1);
 plot(upperEnv);
